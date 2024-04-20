89752986e6126e8dc301cf1041e7132dbe246438
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-04-20 18:20:48 +0800

    Update translate-echo.js

--- translate-echo.js
+++ translate-echo.js
@@ -1,21 +1,10 @@
-let tags=['h1','h2','h3','h4','h5','h6','p','span','strong'];
-for (k=0;k<tags.length;k++){
-let all=document.body.querySelectorAll('td');console.log(all.length);
+javascript: (async()=>{
+let all=document.body.querySelectorAll('.tl');
 for (var i=0;i<all.length;i++){
-    if(all[i].tagName!=='div'){
-    if(all[i].className!=='elmo'){
-        var a=all[i].textContent;
-    if(a!==''){
-    var tn=all[i].tagName.toLowerCase();console.log(tn);
-    var t=await translate(a);
-    var tTag=document.createElement(tn);
-        tTag.className='elmo';
-        tTag.style.color='grey';
-    tTag.textContent=t;console.log(tTag);console.log(all[i].nextSibling);
-    all[i].parentNode.insertBefore(tTag,all[i].nextSibling);
-    }
+var t=await translate(a);
+all[i].innerHTML=all[i].innerHTML+'<br>'+t;
 }
-}}}
+
 async function translate(a){
 var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=en&tl=zh-TW&q='+a.replaceAll('%','%25');
 var res=await fetch(url);
@@ -26,3 +15,4 @@ for (var j=0;j<raw[0].length;j++){
 } 
     return ts
 }
+})();
