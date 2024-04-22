9a043156cc97330e1f27725314a732ab37369cc5
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-04-22 11:00:56 +0800

    Update translate-echo.js

--- translate-echo.js
+++ translate-echo.js
@@ -6,7 +6,7 @@ all[i].innerHTML=all[i].innerHTML+'<br>'+t;
 }
 
 async function translate(a){
-var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=auto&tl=zh-TW&q='+a.replace(/\%/g,'%25').replace(/\#/g,'%23');
+var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=auto&tl=zh-TW&q='+a.replaceAll('%','%25').replaceAll('#','%23');
 var res=await fetch(url);
 var raw=await res.json();
 var ts='';
