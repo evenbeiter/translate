94133f77d272216f914761eb60283c238768627a
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-04-22 13:32:32 +0800

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,10 +1,44 @@
+
+
+let tags=['h1','h2','h3','h4','h5','h6','p','span','strong','a'];
+for (k=0;k<tags.length;k++){
+let all=document.body.querySelectorAll(tags[k]);console.log(all.length);
+for (var i=0;i<all.length;i++){
+    var a=all[i].innerText;
+        if(a!==''){
+            var t=await translate(a);
+            all[i].innerHTML=all[i].innerHTML+'<br>'+t;
+        }
+    }
+}
+
+async function translate(a){
+var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=auto&tl=zh-TW&q='+escape(a);
+var res=await fetch(url);
+var raw=await res.json();
+var ts='';
+for (var j=0;j<raw[0].length;j++){
+    ts=ts+raw[0][j][0];
+} 
+    return ts
+}
+
+
+
+
+
+
+//////////////////////////////////////
+
+
+
 let tags=['h1','h2','h3','h4','h5','h6','p','span','strong'];
 for (k=0;k<tags.length;k++){
 let all=document.body.querySelectorAll(tags[k]);console.log(all.length);
 for (var i=0;i<all.length;i++){
     if(all[i].tagName!=='div'){
     if(all[i].className!=='elmo'){
-        var a=all[i].textContent.replaceAll('%','%25');
+        var a=all[i].innerText;
     if(a!==''){
     var tn=all[i].tagName.toLowerCase();console.log(tn);
     var t=await translate(a);
@@ -17,7 +51,7 @@ for (var i=0;i<all.length;i++){
 }
 }}}
 async function translate(a){
-var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=auto&tl=zh-TW&q='+a;
+var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=auto&tl=zh-TW&q='+escape(a);
 var res=await fetch(url);
 var raw=await res.json();
 var ts='';
