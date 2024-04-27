c977152793e2f28a69dc838394fa897cb41b6d58
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-04-27 21:05:04 +0800

    Update translate-echo.js

--- translate-echo.js
+++ translate-echo.js
@@ -1,9 +1,10 @@
 javascript: (async()=>{
 let all=document.body.querySelectorAll('.tl');
 for (var i=0;i<all.length;i++){
+if (all[i].innerText!==''){
 var t=await translate(all[i].innerText);
 all[i].innerHTML=all[i].innerHTML+'<br>'+t;
-}
+}}
 
 async function translate(a){
 var url = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=auto&tl=zh-TW&q='+encodeURIComponent(a);
